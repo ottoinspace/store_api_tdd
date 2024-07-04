aea840cd90986220a8f179057ace1f9b021df62f
Arnoldo Bezerra Pinheiro <arnoldobepi@gmail.com>
2024-07-04 17:57:35 -0300

    feat: add configs

--- store/usecases/product.py
+++ store/usecases/product.py
@@ -1,7 +1,12 @@
+from typing import List
+from uuid import UUID
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase  # type: ignore
 
+import pymongo  # type: ignore
+
+from store.core.exceptions import NotFoundException
 from store.db.mongo import db_client
-from store.schemas.product import ProductIn, ProductOut
+from store.schemas.product import ProductIn, ProductOut, ProductUpdate, ProductUpdateOut
 
 
 class ProductUseCase:
@@ -11,7 +16,30 @@ class ProductUseCase:
         self.collection = self.database.get_collection("products")
 
     async def create(self, body: ProductIn) -> ProductOut:
-        await self.collection.insert_one(body.model_dump())
+        product = ProductOut(**body.model_dump())
+        await self.collection.insert_one(product.model_dump())
+
+        return product
+
+    async def get(self, id: UUID) -> ProductOut:
+        result = await self.collection.find_one({"id": id})
+
+        if not result:
+            raise NotFoundException(message=f"Product not found with filter: {id}")
+
+        return ProductOut(**result)
+
+    async def query(self) -> List[ProductOut]:
+        return [ProductOut(**item) async for item in self.collection.find()]
+
+    async def update(self, id: UUID, body: ProductUpdate) -> ProductUpdateOut:
+        result = await self.collection.find_one_and_update(
+            filter={"id": id},
+            update={"$set": body.model_dump(exclude_none=True)},
+            return_document=pymongo.ReturnDocument.AFTER,
+        )
+
+        return ProductUpdateOut(**result)
 
 
 product_usecase = ProductUseCase()
--- tests/usecases/test_product.py
+++ tests/usecases/test_product.py
@@ -1,4 +1,10 @@
-from store.schemas.product import ProductOut
+from typing import List
+import pytest  # type: ignore
+from uuid import UUID
+
+
+from store.core.exceptions import NotFoundException
+from store.schemas.product import ProductOut, ProductUpdateOut
 from store.usecases.product import product_usecase
 
 
@@ -6,3 +12,34 @@ async def test_usecases_should_return_sucess(product_in):
     result = await product_usecase.create(body=product_in)
 
     assert isinstance(result, ProductOut)
+    assert result.name == "Iphone 14 pro Max"
+
+
+async def test_usecases_get_should_return_sucess(product_id):
+    result = await product_usecase.get(id=product_id)
+
+    assert isinstance(result, ProductOut)
+    assert result.name == "Iphone 14 pro Max"
+
+
+async def test_usecases_get_should_not_found():
+    with pytest.raises(NotFoundException) as err:
+        await product_usecase.get(id=UUID("1e4f214e-85f7-461a-89d0-a751a32e3bb9"))
+
+    assert (
+        err.value.message
+        == "Product not found with filter: 1e4f214e-85f7-461a-89d0-a751a32e3bb9"
+    )
+
+
+async def test_usecases_query_should_return_sucess():
+    result = await product_usecase.query()
+
+    assert isinstance(result, List)
+
+
+async def test_usecases_update_should_return_sucess(product_id, product_up):
+    product_up.price = 7.500
+    result = await product_usecase.update(id=product_id, body=product_up)
+
+    assert isinstance(result, ProductUpdateOut)
